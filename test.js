becec9b2aae8e15ce1a2ede11dd7efacd2bcbfe6
David Bender <codehero@yahoo.com>
2013-06-20 16:21:41 -0400

    added test script

---
+++ test.js
@@ -0,0 +1,34 @@
+var util = require("util");
+var couchdb = require("couchdb-api");
+var fs = require("fs");
+var CommentTree = require("./comments.js");
+
+if(process.argv.length < 3){
+	console.error("Must specify configuration file.");
+	process.exit(1);
+}
+
+var CONFIG = JSON.parse(fs.readFileSync(process.argv[2]));
+
+var client = couchdb.srv("http://"+ CONFIG.host +":"+ CONFIG.port);
+client.auth = CONFIG.user +":"+ CONFIG.password;
+var cdb = client.db(CONFIG.dbName);
+
+var treeDB = new CommentTree.CommentTree(cdb);
+/*
+treeDB.addNode({"test":2}, 10, function(err, newID){
+	if(err){
+		util.debug("ADD NODE ERROR: " + JSON.stringify(err));
+		return;
+	}
+	*/
+
+	treeDB.getTree([10], -1, function(err, data){
+		if(err){
+			util.debug("GET TREE ERROR: " + JSON.stringify(err));
+			return;
+		}
+
+		console.log("FINAL ANSER" + JSON.stringify(data));
+	});
+//});
